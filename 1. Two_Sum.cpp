a1000287d358b573d7e29b9965ac2409a261a430
Arthur <64936353+av9c@users.noreply.github.com>
2020-05-21 20:20:25 +0300

    some layouts 2

--- 1. Two_Sum.cpp
+++ 1. Two_Sum.cpp
@@ -113,16 +113,21 @@ stored in the hash table, which stores exactly nn elements.
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // Approach 2: Two-pass Hash Table: Hashmap solution to C, top 1%, 4ms runtime.             ///
 ///////////////////////////////////////////////////////////////////////////////////////////////
-struct node {
+struct node 
+{
     int key;
     int val;
     struct node* next;
 };
-struct table {
+
+struct table 
+{
     int size;
     struct node** list;
 };
-struct table* createTable( int size ) {
+
+struct table* createTable( int size ) 
+{
     struct table* t = ( struct table* )malloc( sizeof( struct table ) );
     t->size = size;
     t->list = ( struct node** )malloc( sizeof( struct node* ) * size );
@@ -131,18 +136,24 @@ struct table* createTable( int size ) {
         t->list[i] = NULL;
     return t;
 }
-int hashCode( struct table* t, int key ) {
+
+int hashCode( struct table* t, int key ) 
+{
     if( key < 0 )
         return -( key % t->size );
     return key % t->size;
 }
-void insert( struct table* t, int key, int val ) {
+
+void insert( struct table* t, int key, int val ) 
+{
     int pos = hashCode( t, key );
     struct node* list = t->list[pos];
     struct node* newNode = ( struct node* )malloc( sizeof( struct node ) );
     struct node* temp = list;
-    while( temp ) {
-        if( temp->key == key ) {
+    while( temp ) 
+    {
+        if( temp->key == key ) 
+        {
             temp->val = val;
             return;
         }
@@ -153,30 +164,36 @@ void insert( struct table* t, int key, int val ) {
     newNode->next = list;
     t->list[pos] = newNode;
 }
-int lookup( struct table* t, int key ) {
+
+int lookup( struct table* t, int key ) 
+{
     int pos = hashCode( t, key );
     struct node* list = t->list[pos];
     struct node* temp = list;
-    while( temp ) {
-        if( temp->key == key ) {
+    while( temp ) 
+    {
+        if( temp->key == key ) 
             return temp->val;
-        }
+        
         temp = temp->next;
     }
     return -1;
 }
 
-int* twoSum_TwoPassHashTableToC( int* nums, int numsSize, int target, int* returnSize ) {
+int* twoSum_TwoPassHashTableToC( int* nums, int numsSize, int target, int* returnSize ) 
+{
     struct table* t = createTable( numsSize );
     int i;
     *returnSize = 2;
     int* result = (int*)malloc( sizeof( int ) * *returnSize );
-    for( i = 0; i < numsSize; i++ ) {
+    for( i = 0; i < numsSize; i++ ) 
         insert( t, nums[i], i );
-    }
-    for( i = 0; i < numsSize; i++ ) {
+
+    for( i = 0; i < numsSize; i++ ) 
+    {
         int complement = target - nums[i];
-        if( lookup( t, complement ) != -1 && lookup( t, complement ) != i ) {
+        if( lookup( t, complement ) != -1 && lookup( t, complement ) != i ) 
+        {
             result[0] = lookup( t, complement );
             result[1] = i;
         }
@@ -195,7 +212,8 @@ vector<int> twoSum_TwoPassHashTableToModernCPP( vector<int>& nums, int target )
     for( size_t i = 0; i < nums.size(); ++i ) 
         map.emplace( std::make_pair( nums.at( i ), i ) );
     
-    for( size_t i = 0; i < nums.size(); ++i ) {
+    for( size_t i = 0; i < nums.size(); ++i ) 
+    {
         const auto mit = map.find( target - nums.at( i ) );
         if( mit != map.end() && (int)i != mit->second ) 
             return { (int)i, mit->second };
